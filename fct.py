65487c78dccf2b62867109cc7a9bada3555193dd
MBA <mathieu.bazin@eepi-robotics.com>
2022-07-25 15:55:00 +0200

    Client Added

--- fct.py
+++ fct.py
@@ -263,40 +263,90 @@ def Mer_Pol(webhook,HH,MM,test_mode,creds):
 
     data = responses.get_all_records()
 
-    total = 0
+    total_EEPI = 0
+    total_Client = 0
     petites = []
+    petites_clients = []
     grandes = []
+    grandes_clients = []
+    commande_EEPI = []
+    commande_Clients = []
     commande_finale = []
 
     for element in data:
-        if element['Votre Taille :'] == "Petite":
-            if element['Suppléments :'] != "":
-                price = pizzas[element['Votre Pizza :']][0]+1
-            else :
-                price = pizzas[element['Votre Pizza :']][0]
-            commande = f"{element['Qui êtes-vous ?']} - {element['Votre Taille :']} {element['Votre Pizza :']} | Avec : {element['Suppléments :']} | Sans : {element['Sans : ']} | Total : {price}€"
-            petites.append(commande)
-        else:
-            if element['Suppléments :'] != "":
-                price = pizzas[element['Votre Pizza :']][1]+1
-            else :
-                price = pizzas[element['Votre Pizza :']][1]
-            commande = f"{element['Qui êtes-vous ?']} - {element['Votre Taille :']} {element['Votre Pizza :']} | Avec : {element['Suppléments :']} | Sans : {element['Sans : ']} | Total : {price}€"
-            grandes.append(commande)
+        if element['Qui êtes-vous ?'] == 'Client':
+            if element['Votre Taille :'] == "Petite":
+                if element['Suppléments :'] != "":
+                    price_Client = pizzas[element['Votre Pizza :']][0]+1
+                else :
+                    price_Client = pizzas[element['Votre Pizza :']][0]
+                commande = f"{element['Votre Taille :']} {element['Votre Pizza :']} | Avec : {element['Suppléments :']} | Sans : {element['Sans : ']} | Total : {price_Client}€"
+                petites_clients.append(commande)
+            else:
+                if element['Suppléments :'] != "":
+                    price_Client = pizzas[element['Votre Pizza :']][1]+1
+                else :
+                    price_Client = pizzas[element['Votre Pizza :']][1]
+                commande = f"{element['Votre Taille :']} {element['Votre Pizza :']} | Avec : {element['Suppléments :']} | Sans : {element['Sans : ']} | Total : {price_Client}€"
+                grandes_clients.append(commande)
+            total_Client = total_Client + price_Client
+        else : 
+            if element['Votre Taille :'] == "Petite":
+                if element['Suppléments :'] != "":
+                    price_EEPI = pizzas[element['Votre Pizza :']][0]+1
+                else :
+                    price_EEPI = pizzas[element['Votre Pizza :']][0]
+                commande = f"{element['Qui êtes-vous ?']} - {element['Votre Taille :']} {element['Votre Pizza :']} | Avec : {element['Suppléments :']} | Sans : {element['Sans : ']} | Total : {price_EEPI}€"
+                petites.append(commande)
+            else:
+                if element['Suppléments :'] != "":
+                    price_EEPI = pizzas[element['Votre Pizza :']][1]+1
+                else :
+                    price_EEPI = pizzas[element['Votre Pizza :']][1]
+                commande = f"{element['Qui êtes-vous ?']} - {element['Votre Taille :']} {element['Votre Pizza :']} | Avec : {element['Suppléments :']} | Sans : {element['Sans : ']} | Total : {price_EEPI}€"
+                grandes.append(commande)
+            total_EEPI = total_EEPI + price_EEPI
+    
+    total = total_EEPI + total_Client
 
-        total = total + price
-        
+    
     for element in grandes:
-        commande_finale.append(element)
+        commande_EEPI.append(element)
     
     for element in petites:
+        commande_EEPI.append(element)
+
+    for element in grandes_clients:
+        commande_Clients.append(element)
+
+    for element in petites_clients:
+        commande_Clients.append(element)
+
+    commande_finale.append("---------- Commande EEPI ----------")
+    for element in commande_EEPI :
         commande_finale.append(element)
+    commande_finale.append(f"Nombre de pizzas EEPI : {len(commande_EEPI)}")
+    commande_finale.append(f"Total EEPI : {total_EEPI}€")
+
+    if len(commande_Clients)>0:
+        commande_finale.append("---------- Commande Client ----------")
+        for element in commande_Clients:
+            commande_finale.append(element)
+        commande_finale.append("DEMANDER FACTURE !")
+        commande_finale.append(f"Nombre de pizzas client : {len(commande_Clients)}")
+        commande_finale.append(f"Total client : {total_Client}€")
+        commande_finale.append("-----------------------------------")
+    else :
+        commande_finale.append("----------------------------------")
 
     result = ""
+
     for element in commande_finale:
         result = result + str(element)+"   \n   \n"
+
     
-    result = result + f"Nombre de pizza : {len(data)}   \n"
+    
+    result = result + f"Nombre total de pizzas : {len(data)}   \n"
     result = result + f"Total : {total}€"
 
     data = {
